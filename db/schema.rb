cbd84980d7327e1a21998fdcd9b072e0f66aa802
Svezhak <problema2000@gmail.com>
2017-11-16 04:03:49 +0100

    migration: add time_zone to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112211138) do
+ActiveRecord::Schema.define(version: 20171116030215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20171112211138) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.string "time_zone", default: "UTC"
     t.index ["token"], name: "index_users_on_token"
   end
 
