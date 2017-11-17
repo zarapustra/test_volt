47befeee6a39e356e9e94848f478ad05fd01701a
Svezhak <problema2000@gmail.com>
2017-11-18 00:35:01 +0100

    refactored command generate_report

--- spec/core/report/command/generate_report_spec.rb
+++ spec/core/report/command/generate_report_spec.rb
@@ -1,56 +1,49 @@
 require 'rails_helper'
 
 describe Report::Command::GenerateReport, type: :command do
-  dates = [*1.month.ago.to_date..Date.today]
-  params = {
-    start_date: dates[0].to_s,
-    end_date: dates[-1].to_s,
-    email: 'reports@example.com'
-  }
+  subject { Report::Command::GenerateReport.call(@params) }
 
-  it 'produces valid report' do
-    # seeding
-    user1 = create(:user)
-    posts1 = 3.times.map { create(:post, user: user1, published_at: dates.sample) }
+  before(:all) do
+    @users = users = 4.times.map { create(:user) }
+    dates = [*1.month.ago.to_date..Date.today]
+    posts1 = 3.times.map { create(:post, user: users[0], published_at: dates.sample) }
     15.times { create(:comment, user_id: User.ids.sample, post: posts1.sample) }
 
-    user2 = create(:user)
-    posts2 = 2.times.map { create(:post, user: user2, published_at: dates.sample) }
+    posts2 = 2.times.map { create(:post, user: users[1], published_at: dates.sample) }
     21.times { create(:comment, user_id: User.ids.sample, post: posts2.sample) }
 
-    expect { Report::Command::GenerateReport.call(params) }.to broadcast(:ok)
-    rows = CSV.read(Dir['tmp/Report-by-author*.csv'].last).drop(1)
-    expect(rows.size).to eq(2)
-    expect(rows.first).to eq([user1.nickname, user1.email, '3', '15'])
-    expect(rows.last).to eq([user2.nickname, user2.email, '2', '21'])
-
-    user3 = create(:user)
-    posts3 = 3.times.map { create(:post, user: user3, published_at: dates.sample) }
+    posts3 = 3.times.map { create(:post, user: users[2], published_at: dates.sample) }
     5.times { create(:comment, user_id: User.ids.sample, post: posts3.sample) }
 
-    expect { Report::Command::GenerateReport.call(params) }.to broadcast(:ok)
-    rows = CSV.read(Dir['tmp/Report-by-author*.csv'].last).drop(1)
-    expect(rows.first[0]).to eq(user1.nickname)
-    expect(rows.last[0]).to eq(user3.nickname)
-
-    user4 = create(:user)
-    posts4 = 1.times.map { create(:post, user: user4, published_at: dates.sample) }
+    posts4 = 1.times.map { create(:post, user: users[3], published_at: dates.sample) }
     40.times { create(:comment, user_id: User.ids.sample, post: posts4.sample) }
 
-    expect { Report::Command::GenerateReport.call(params) }.to broadcast(:ok)
+    @params = {
+      start_date: dates[0].to_s,
+      end_date: dates[-1].to_s,
+      email: 'reports@example.com'
+    }
+  end
+
+  it 'is ok' do
+    expect { subject }.to broadcast(:ok)
+  end
+
+  it 'has valid result' do
+    subject
     rows = CSV.read(Dir['tmp/Report-by-author*.csv'].last).drop(1)
-    expect(rows.first[0]).to eq(user4.nickname)
-    expect(rows.last[0]).to eq(user3.nickname)
+    expected_nicknames = [3, 0, 1, 2].map { |i| @users[i].nickname }
+    expect(rows.map { |row| row.first }).to eq(expected_nicknames)
   end
 end
 
 =begin
 
-user  (posts_count + comments/10.0) =  rating
+user  (posts_count + comments/10.0) ==  rating
 ------------------------------------------------
-user4      1       +     40 / 10.0  =     5
-user1      3       +     15 / 10.0  =   4,5
-user2      2       +     21 / 10.0  =   4,1
-user3      3       +      5 / 10.0  =   3.5
+user4      1       +     40 / 10.0  ==     5
+user1      3       +     15 / 10.0  ==   4,5
+user2      2       +     21 / 10.0  ==   4,1
+user3      3       +      5 / 10.0  ==   3.5
 
 =end
