054465e9f46bb497cfab959706d14bee1115c463
Svezhak <problema2000@gmail.com>
2017-12-17 01:55:50 +0100

    modify application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,23 @@
 require_relative 'boot'
+require 'rails'
 
-require 'rails/all'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+# require 'action_view/railtie'
+require 'action_cable/engine'
+# require 'sprockets/railtie'
+# require 'rails/test_unit/railtie'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Volt
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths += Dir["#{config.root}/lib", "#{config.root}/lib/**/"]
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.api_only = true
 
     Time::DATE_FORMATS[:datetime] = '%d.%m.%Y %H:%M:%S'
     Time::DATE_FORMATS[:time] = '%H:%M:%S'
