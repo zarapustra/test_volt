c532ae97803671c4521a827af91e5d093f91bd56
Svezhak <problema2000@gmail.com>
2018-01-19 13:27:51 +0100

    Refactor User update form

--- app/core/user/form/update_form.rb
+++ app/core/user/form/update_form.rb
@@ -4,29 +4,29 @@ class User::Form::UpdateForm < Rectify::Form
   attribute :avatar, String
   #TODO change email and password separately in other commands
 
-  validates :nickname, format: {without: /\s/}
-  validate :nickname_unique?
-
   private
 
   def before_validation
+    validate_nickname
+    validate_avatar
+  end
+
+  def validate_nickname
+    self.nickname = context.user.nickname unless nickname.present?
+    case
+    when same_nickname?
+      errors.add(:nickname, 'New nickname is the same as old')
+    when nickname_in_use?
+      errors.add(:nickname, 'Already in use')
+    when nickname_with_spaces?
+      errors.add(:nickname, 'Got spaces')
+    else
+      downcase_nickname!
+    end
+  end
+
+  def validate_avatar
     assign_avatar!
-    downcase_nickname!
-  end
-
-  def assign_avatar!
-    #TODO rescue
-    return unless avatar.present?
-    self.avatar = Base64Image.file_decode(avatar.split(',')[1], "#{Time.now.to_i}.jpg")
-  end
-
-  def downcase_nickname!
-    self.nickname = nickname&.downcase
-  end
-
-  def nickname_unique?
-    return if nickname.nil? || same_nickname? || (not nickname_in_use?)
-    errors.add(:nickname, 'Already in use')
   end
 
   def same_nickname?
@@ -35,6 +35,21 @@ class User::Form::UpdateForm < Rectify::Form
 
   def nickname_in_use?
     User.find_by(nickname: nickname)
+  end
+
+  def nickname_with_spaces?
+    nickname =~ /\s/
+  end
+
+  def downcase_nickname!
+    self.nickname = nickname&.downcase
+  end
+
+  def assign_avatar!
+    return unless avatar.present?
+    self.avatar = Base64Image.file_decode(avatar.split(',')[1], "user_id.#{context.user.id}-time.#{Time.now.to_i}.jpg")
+  rescue => e
+    errors.add(:nickname, "Server error: Base64 decoding went wrong: #{e}")
   end
 end
 
